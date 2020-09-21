06a590225420c318cde7de6b890c289eb67285ce
Francisco Correia28 <francisco.correia1804@gmail.com>
2020-09-21 01:51:07 +0100

    uploading

---
+++ build/precache-manifest.c2f9fa5c342367de495b9b755a0e53fb.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f057bec7a0f82b50ea2202d5370a76c",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "efd4d7815b11b27c515d",
+    "url": "/ecommerce/static/css/main.7178986c.chunk.css"
+  },
+  {
+    "revision": "94f5583930b527e1e0e7",
+    "url": "/ecommerce/static/js/2.a56c24a7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ecommerce/static/js/2.a56c24a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efd4d7815b11b27c515d",
+    "url": "/ecommerce/static/js/main.6e17dbb6.chunk.js"
+  },
+  {
+    "revision": "831a87ab3375c7c427c2",
+    "url": "/ecommerce/static/js/runtime-main.ee40b3dc.js"
+  },
+  {
+    "revision": "37a3300fd30fa69263a3e61e7575f0ee",
+    "url": "/ecommerce/static/media/Lato-BoldItalic.37a3300f.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ecommerce/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ecommerce/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ecommerce/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ecommerce/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ecommerce/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "56b287dc90ecb0a78c852d187af88b84",
+    "url": "/ecommerce/static/media/hotdeal.56b287dc.png"
+  },
+  {
+    "revision": "dc3a1003fab94c2426389ef871bcef09",
+    "url": "/ecommerce/static/media/product01.dc3a1003.png"
+  },
+  {
+    "revision": "136739e8f6eaf02ce22aaf76ac692f6e",
+    "url": "/ecommerce/static/media/product02.136739e8.png"
+  },
+  {
+    "revision": "eb2b45c7d6206e71e40b18d3e79e4282",
+    "url": "/ecommerce/static/media/product03.eb2b45c7.png"
+  },
+  {
+    "revision": "a4f7bb6e9efb39bb34671a707d10f691",
+    "url": "/ecommerce/static/media/product04.a4f7bb6e.png"
+  },
+  {
+    "revision": "a4a81a623a8a5cc5bf39cc5aa1e56efa",
+    "url": "/ecommerce/static/media/product05.a4a81a62.png"
+  },
+  {
+    "revision": "c6df9a0269f93069f879eb0422dc97af",
+    "url": "/ecommerce/static/media/product06.c6df9a02.png"
+  },
+  {
+    "revision": "3f731243e7997c7f32fe37e4c8669974",
+    "url": "/ecommerce/static/media/product07.3f731243.png"
+  },
+  {
+    "revision": "30bf47e4e4fafb4ed84810a1377ab1d5",
+    "url": "/ecommerce/static/media/product08.30bf47e4.png"
+  },
+  {
+    "revision": "c15c8d83b0030a303026d619a76dcfda",
+    "url": "/ecommerce/static/media/product09.c15c8d83.png"
+  },
+  {
+    "revision": "14ae1b577540f2382b9fc95fa1006898",
+    "url": "/ecommerce/static/media/shop01.14ae1b57.png"
+  },
+  {
+    "revision": "c1f0c966511c7c847edaa3095be132c0",
+    "url": "/ecommerce/static/media/shop02.c1f0c966.png"
+  },
+  {
+    "revision": "0adf2875fa3122413a4b40670579cb23",
+    "url": "/ecommerce/static/media/shop03.0adf2875.png"
+  }
+]);
