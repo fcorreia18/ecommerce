c009b0eed10f67bede3ea1c1ce6a3984e57e0a22
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-03-20 21:08:20 +0100

    chore: components projects changend to typescript core

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,21 +3,21 @@ import Home from './Home';
 import Store from './Store';
 import Product from './Product';
 import Checkout from './Checkout';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-
+ 
 export default function () {
 
     return (
-        <Router>
+        <BrowserRouter>
             <div>
-                <Switch>
-                    <Route path="/" exact component={Home} />
-                    <Route path="/store" component={Store} />
-                    <Route path="/product" component={Product} />
-                    <Route path="/checkout" component={Checkout} />
-                </Switch>
+                <Routes>
+                    <Route path="/" exact element={<Home/>} />
+                    <Route path="/store" element={<Store/>} />
+                    <Route path="/product" element={<Product/>} />
+                    <Route path="/checkout" element={<Checkout/>} />
+                </Routes>
             </div>
-        </Router>
+        </BrowserRouter>
     );
 }
